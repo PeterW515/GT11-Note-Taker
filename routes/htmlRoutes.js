398a454afda4e45360ba6ba273ada6658a7d2302
Peter Weigman <phweigman@gmail.com>
2021-08-03 21:47:16 -0400

    added html routes in

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = (app) => {
+    //this returns the notes html when the notes api is hit
+    app.get('/notes',(req,res)=>{
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    //this returns index html when any unknown api is hit
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../public/index.html'));
+    });
+}
--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
+//link routes
+//require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
+//start server
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 })
