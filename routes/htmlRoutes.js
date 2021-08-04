e5b2833439bf8ab8c3d6253b07327f1f15ba8773
Peter Weigman <phweigman@gmail.com>
2021-08-03 22:24:15 -0400

    stuck on src issue, stopping for the night

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = (app) => {
-    //this returns the notes html when the notes api is hit
+    //this returns the notes html when the notes endpoint is hit
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
